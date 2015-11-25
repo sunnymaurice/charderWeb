537ddc3f9efcdee5e251d9e962af9cd6648f85e8
PeacePan <minghe0915@gmail.com>
2015-11-25 16:54:07 +0800

    Start bootstrap-work

---
+++ bootstrap-work/src/php/db_config.php
@@ -0,0 +1,12 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "wificm"); // db user
+define('DB_PASSWORD', "wificm"); // db password (mention your db password here)
+define('DB_NAME', "CM_TEST"); // database name
+define('DB_SERVER', "127.0.0.1"); // db server
+
+?>
